efbc64b1afe91bf5f15005a06977fcb734c0b7e7
Daiane Babicz <daibabicz@hotmail.com>
2022-11-25 14:21:59 -0300

    CHAT FEATURE V2

---
+++ app/src/main/java/com/example.chat_psicotinder/RetrofitInstance.kt
@@ -0,0 +1,22 @@
+package com.example.chat_psicotinder
+
+import com.example.chat_psicotinder.Constants.Constants
+import com.example.chat_psicotinder.`interface`.NotificationApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+
+    companion object{
+        private val retrofit by lazy {
+            Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        val api by lazy {
+            retrofit.create(NotificationApi::class.java)
+        }
+    }
+}
